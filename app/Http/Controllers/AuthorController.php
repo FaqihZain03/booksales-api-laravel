7793d6a9c382c7fb24afe4d191c5c969094cc77c
FaqihZain03 <faqihzain03@gmail.com>
2025-05-23 14:00:55 +0700

    pertemuan5 laravel

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -49,5 +49,73 @@ class AuthorController extends Controller
         ], 201);
     }
 
+    public function show(string $id) {
+        $authors = Author::find($id);
+
+        if (!$authors) {
+            return response()->json([
+                'success' => false,
+                'message' => 'resource not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'not detail resource',
+            'data' => $authors
+        ], 200);
+    }
+
+    public function update(string $id, Request $request){
+        $authors = Author::find($id);
+
+        if (!$authors) {
+            return response()->json([
+                'success' => false,
+                'message' => 'resource not found',
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        $data = [
+            'name' => $request->name,
+        ];
+
+        $authors->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'resource updated successfully',
+            'data' => $authors
+        ], 200);
+    }
+    public function destroy(string $id){
+        $authors = Author::find($id);
+
+        if (!$authors) {
+            return response()->json([
+                'success' => false,
+                'message' => 'resource not found',
+            ], 404);
+        }
+
+        $authors->delete();
+
+        return response()->json([
+           'success' => true,
+           'message' => 'resource deleted successfully'
+        ]);
+    }
+
 }
 
--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -50,4 +50,75 @@ class GenreController extends Controller
             'message' => 'Genre Created Successfully',
         ], 201);
     }
+
+    public function show(string $id) {
+        $genres = Genre::find($id);
+
+        if (!$genres) {
+            return response()->json([
+                'success' => false,
+                'message' => 'resource not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'not detail resource',
+            'data' => $genres
+        ], 200);
+    }
+
+
+    public function update(string $id, Request $request){
+        $genres = Genre::find($id);
+
+        if (!$genres) {
+            return response()->json([
+                'success' => false,
+                'message' => 'resource not found',
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:100',
+            'description' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => $validator->errors()
+            ], 422);
+        }
+
+        $data = [
+            'name' => $request->name,
+            'description' => $request->description,
+        ];
+
+        $genres->update($data);
+
+        return response()->json([
+            'success' => true,
+            'message' => 'resource updated successfully',
+            'data' => $genres
+            ], 200);
+    }
+    public function destroy(string $id){
+        $genres = Genre::find($id);
+
+        if (!$genres) {
+            return response()->json([
+                'success' => false,
+                'message' => 'resource not found',
+            ], 404);
+        }
+
+        $genres->delete();
+
+        return response()->json([
+           'success' => true,
+           'message' => 'resource deleted successfully'
+        ]);
+    }
 }
